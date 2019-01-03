0a1b35bad75fd971775f13ba82e112cefdbf7962
Kyle <polysplitter@gmail.com>
2019-01-02 18:23:59 -0600

    first ;

---
+++ index.js
@@ -0,0 +1,31 @@
+'use-strict'
+const express = require('express')
+const request = require('request')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res, next) => {
+    res.sendfile(__dirname + "/index.html")
+})
+
+app.post('/', (req, res, next) => {
+    let crypto = req.body.crypto
+    let fiat = req.body.fiat
+    
+    request(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${crypto+fiat}`, (error, response, body) => {
+        const data = JSON.parse(body)
+        let price = data.last
+
+        let currentDate = data.display_timestamp
+
+        res.write(`<p>The current date is ${currentDate}</p>`)
+        res.write(`<h1>The price of ${crypto} is: ${price} ${fiat}</h1>`)
+        res.send()
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running on port: 3000')
+})
---
+++ math.test.js
@@ -0,0 +1,6 @@
+function two(one, two) {
+    return one + two
+}
+test("Adding 1 + 1 equals 2", () => {
+    expect(two(1, 1).toBe(2))
+  })
